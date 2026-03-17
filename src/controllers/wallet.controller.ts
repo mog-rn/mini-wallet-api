fccdf1fff1bc86f796542ff0240ce3dac9610061
mog-rn <mogaka.amo254@gnail.com>
2026-03-17 11:55:43 +0300

    feat: wallet api setup

--- src/controllers/wallet.controller.ts
+++ src/controllers/wallet.controller.ts
@@ -1,15 +1,47 @@
 import type { Request, Response, NextFunction } from "express";
 import * as walletService from "../services/wallet.services.ts";
+import {z} from "zod";
+import { BadRequestError } from "../utils/error.ts";
+
+const createWalletSchema = z.object({
+    ownerName: z.string()
+    .min(1, "Owner name is required")
+    .max(255, "Owner name must be less than 255 characters")
+    .trim()
+});
+
 
 export const createWallet = async (req: Request, res: Response, next: NextFunction) => {
-    
+    try {
+        const result = createWalletSchema.safeParse(req.body);
+
+        if (!result.success) {
+            throw new BadRequestError(result.error.issues.map(issue => issue.message).join(", "));
+        }
+
+        const wallet = await walletService.create(result.data.ownerName);
+
+        res.status(201).json(wallet);
+    } catch (error) {
+        next(error);
+        
+    }
 }
 
-export const getWallet = async (req: Request, res: Response, next: NextFunction) => {
-    
+export const getWallet = async (req: Request<{ id: string}>, res: Response, next: NextFunction) => {
+    try {
+        const wallet = await walletService.findOne(req.params.id);
+        res.status(200).json(wallet);
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const getAllWallets = async (req: Request, res: Response, next: NextFunction) => {
-    
+    try {
+        const wallets = await walletService.findAll();
+        res.status(200).json(wallets);
+    } catch (error) {
+        next(error);
+    }   
 }
-
--- src/services/wallet.services.ts
+++ src/services/wallet.services.ts
@@ -1,13 +1,21 @@
 import {prisma} from "../lib/prisma.ts";
 
 export const create = async (ownerName: string) => {
-
+    return await prisma.wallet.create({
+        data: {
+            ownerName
+        }
+    });
 }
 
 export const findAll = async () => {
-
+    return await prisma.wallet.findMany();
 }
 
-export const findOne = async (id: number) => {
-
+export const findOne = async (id: string) => {
+    return await prisma.wallet.findUnique({
+        where: {
+            id
+        }
+    });
 }
