479edabd154a9824f3458df81b4bb2e4221cbd27
mog-rn <mogaka.amo254@gnail.com>
2026-03-17 12:15:29 +0300

    feat: done with transaction api logic

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -1,16 +1,62 @@
 import type { Request, Response, NextFunction } from "express";
 import * as transactionService from "../services/transaction.services.ts";
+import z from "zod";
+import { BadRequestError } from "../utils/error.ts";
+
+const depositSchema = z.object({
+    walletId: z.uuid("Invalid wallet ID"),
+    amount: z.number()
+    .positive("Amount must be greater than zero")
+    .max(1_000_000, "Amount exceeds maximum limit."),
+    reference: z.string()
+    .optional(),
+})
+
+const transferSchema = z.object({
+    fromWalletId: z.uuid("Invalid sender wallet ID"),
+    toWalletId: z.uuid("Invalid receiver wallet ID"),
+    amount: z.number()
+    .positive("Amount must be greater than zero")
+    .max(1_000_000, "Amount exceeds maximum limit."),
+    reference: z.string()
+    .optional(),
+})
 
 export const deposit = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = depositSchema.safeParse(req.body);
+        if (!result.success) {
+            throw new BadRequestError(result.error.issues.map(issue => issue.message).join(", "));
+        }
+        const transaction = await transactionService.deposit(result.data.walletId, result.data.amount, result.data.reference);
+        res.status(201).json(transaction);
     
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const transfer = async (req: Request, res: Response, next: NextFunction) => {
-
+    try {
+        const result = transferSchema.safeParse(req.body);
+        if (!result.success) {
+            throw new BadRequestError(result.error.issues.map(issue => issue.message).join(", "));
+        }
+        const transaction = await transactionService.transfer(result.data.fromWalletId, result.data.toWalletId, result.data.amount, result.data.reference);
+        res.status(201).json(transaction);
+    } catch (error) {
+        next(error);
+    }
 }
 
 export const getTransactions = async (req: Request, res: Response, next: NextFunction) => {
-
+    try {
+        const walletId = req.query.walletId as string;
+        const transactions = await transactionService.findAll(walletId);
+        res.status(200).json(transactions);
+    } catch (error) {
+        next(error);
+    }
 }
 
 
--- src/services/transaction.services.ts
+++ src/services/transaction.services.ts
@@ -1,13 +1,122 @@
+import { Decimal } from "../../generated/prisma/internal/prismaNamespaceBrowser.ts";
 import {prisma} from "../lib/prisma.ts";
+import { BadRequestError, NotFoundError } from "../utils/error.ts";
 
-export const deposit = async (walletId: number, amount: number, reference?: string) => {
-    
+export const deposit = async (walletId: string, amount: number, reference?: string) => {
+    return await prisma.$transaction(async (tx) => {
+        // Ensure that the wallet exists
+        const wallet = await tx.wallet.findUnique({
+            where: { id: walletId },
+        })
+
+        if (!wallet) {
+            throw new NotFoundError('Wallet');
+        }
+
+        // update the wallet balance
+        await tx.wallet.update({
+            where: { id: walletId },
+            data: {
+                balance: {
+                    increment: amount,
+                }
+            }
+        })
+
+        // create the transaction record for the deposit
+        const transaction = await tx.transaction.create({
+            data: {
+                type: 'DEPOSIT',
+                amount,
+                status: 'SUCCESSFUL',
+                toWalletId: walletId,
+                reference: reference ?? null,
+            }
+        })
+        return transaction;
+    })
 }
 
-export const transfer = async (fromWalletId: number, toWalletId: number, amount: number, reference?: string) => {
+export const transfer = async (fromWalletId: string, toWalletId: string, amount: number, reference?: string) => {
+    return await prisma.$transaction(async (tx) => {
+        // Ensure that the sender wallet exists
+        const sender = await tx.wallet.findUnique({
+            where: { id: fromWalletId },
+        })
 
+        if (!sender) {
+            throw new NotFoundError('Sender wallet');
+        }
+
+        // Ensure that the receiver wallet exists
+        const receiver = await tx.wallet.findUnique({
+            where: { id: toWalletId },
+        })
+        if (!receiver) {
+            throw new NotFoundError('Receiver wallet');
+        }
+
+
+        // Ensure that the sender has sufficient balance
+        if (sender.balance.lessThan(new Decimal(amount))) {
+            await tx.transaction.create({
+                data: {
+                    type: 'TRANSFER',
+                    amount,
+                    status: 'FAILED',
+                    fromWalletId,
+                    toWalletId,
+                    reference: reference ?? null,
+                }
+            })
+            throw new BadRequestError('Insufficient balance');
+        }
+
+        // Debit the sender's wallet
+        await tx.wallet.update({
+            where: { id: fromWalletId },
+            data: {
+                balance: {
+                    decrement: amount,
+                }
+            }
+        })
+
+        // Credit the receiver's wallet
+        await tx.wallet.update({
+            where: { id: toWalletId },
+            data: {
+                balance: {
+                    increment: amount,
+                }
+            }
+        })
+
+        // create the transaction record for the transfer
+        const transaction = await tx.transaction.create({
+            data: {
+                type: 'TRANSFER',
+                amount,
+                status: 'SUCCESSFUL',
+                fromWalletId,
+                toWalletId
+            }
+        })
+        return transaction;
+    })
 }
 
-export const findAll = async (walletId?: number) => {
-
+export const findAll = async (walletId?: string) => {
+    const whereClause = walletId ? {
+        OR: [
+            { fromWalletId: walletId },
+            { toWalletId: walletId }
+        ]
+    } : {};
+    return await prisma.transaction.findMany({
+        where: whereClause,
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
 }
